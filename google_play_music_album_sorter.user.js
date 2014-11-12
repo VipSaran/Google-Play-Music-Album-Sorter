6887bad349d334a20af86bbe8b07426ddce218e8
Robert Saric <robert.saric@gmail.com>
2014-11-12 23:22:04 +0100

    displaying and toggling sort order; sort order images; sorting order is browser-consistant

--- google_play_music_album_sorter.user.js
+++ google_play_music_album_sorter.user.js
@@ -1,29 +1,29 @@
 // ==UserScript==
-// @name           Google Play Music Album Sorter
-// @description    Greasemonkey/Tampermonkey UserScript for extending Google Play Music with album sorting functionality
-// @namespace      http://github.com/VipSaran/Google-Play-Music-Album-Sorter
-// @updateURL      https://github.com/VipSaran/Google-Play-Music-Album-Sorter/raw/master/google_play_music_album_sorter.user.js
-// @version        0.2
-// @author         VipSaran
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @include        http://play.google.com/music/listen*
-// @include        https://play.google.com/music/listen*
-// @include        http://music.google.com/music/listen*
-// @include        https://music.google.com/music/listen*
-// @match          http://play.google.com/music/listen*
-// @match          https://play.google.com/music/listen*
-// @match          http://music.google.com/music/listen*
-// @match          https://music.google.com/music/listen*
-// @run-at         document-end
+// @name          Google Play Music Album Sorter
+// @description   Greasemonkey/Tampermonkey UserScript for extending Google Play Music with album sorting functionality
+// @namespace     http://github.com/VipSaran/Google-Play-Music-Album-Sorter
+// @updateURL     https://github.com/VipSaran/Google-Play-Music-Album-Sorter/raw/master/google_play_music_album_sorter.user.js
+// @version       0.2
+// @author        VipSaran
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @resource      sort_asc https://github.com/VipSaran/Google-Play-Music-Album-Sorter/raw/master/sort_asc.png
+// @resource      sort_desc https://github.com/VipSaran/Google-Play-Music-Album-Sorter/raw/master/sort_desc.png
+// @grant         GM_getResourceURL
+// @include       http://play.google.com/music/listen*
+// @include       https://play.google.com/music/listen*
+// @include       http://music.google.com/music/listen*
+// @include       https://music.google.com/music/listen*
+// @match         http://play.google.com/music/listen*
+// @match         https://play.google.com/music/listen*
+// @match         http://music.google.com/music/listen*
+// @match         https://music.google.com/music/listen*
+// @run-at        document-end
 // ==/UserScript==
 
-var DEBUG = true;
+var DEBUG = false;
 
 function GooglePlayMusicAlbumSorter() {
   if (DEBUG) console.log('GooglePlayMusicAlbumSorter()');
-
-  this.order = this.loadOrder() || 'asc';
-  if (DEBUG) console.log('this.order=', this.order);
 }
 
 GooglePlayMusicAlbumSorter.prototype.loadOrder = function() {
@@ -37,6 +37,7 @@ GooglePlayMusicAlbumSorter.prototype.saveOrder = function(order) {
   window.localStorage.setItem('order', order);
 };
 
+var sectionHeaderText = '';
 var sortingInProgress = false;
 var domModifiedTimeout;
 var domModifiedCallback = function() {
@@ -54,18 +55,39 @@ var domModifiedCallback = function() {
     if (firstSibling && firstSibling.hasClass('section-header')) {
       sortingInProgress = true;
 
+      var sortOrder = sorter.loadOrder() || 'asc';
+      if (DEBUG) console.log('sortOrder=', sortOrder);
+
+      if (sectionHeaderText == '') {
+        sectionHeaderText = firstSibling.html();
+      }
+
       if (DEBUG) console.log('albums=', albums);
       albums.sort(function(a, b) {
         var aYear = $(a).find("a.sub-title").html();
         var bYear = $(b).find("a.sub-title").html();
-        return sorter.order === 'asc' ? aYear - bYear : bYear - aYear;
+        return sortOrder === 'asc' ? aYear - bYear : bYear - aYear;
       });
 
       albumsParent.empty();
-      albumsParent.append('<div class="section-header">Albums</div>');
+      albumsParent.append('<div class="section-header">' + sectionHeaderText + '<span id="gpmas_sorter" class="' + sortOrder + '"></span></div>');
       $.each(albums, function(i, album) {
         // if (DEBUG) console.log('each', i, album);
         albumsParent.append(album);
+      });
+
+      $('#gpmas_sorter').click(function(event) {
+        var currClass = $(this).attr('class');
+        if (DEBUG) console.log('gpmas_sorter click', currClass);
+
+        $(this).toggleClass('asc desc');
+
+        currClass = $(this).attr('class');
+        if (DEBUG) console.log('gpmas_sorter click', currClass);
+
+        sorter.saveOrder(currClass);
+
+        domModifiedCallback();
       });
 
       sortingInProgress = false;
@@ -75,6 +97,14 @@ var domModifiedCallback = function() {
 
 GooglePlayMusicAlbumSorter.prototype.init = function() {
   if (DEBUG) console.log('GooglePlayMusicAlbumSorter.init()');
+
+  $('head').append(
+    '<style>\n' +
+    '#gpmas_sorter {float: right; width: 20px; height: 40px; cursor: pointer; background-repeat: no-repeat; background-position: center center;}\n' +
+    '#gpmas_sorter.asc {background-image: url("' + GM_getResourceURL("sort_asc") + '");}\n' +
+    '#gpmas_sorter.desc {background-image: url("' + GM_getResourceURL("sort_desc") + '");}\n' +
+    '</style>'
+  );
 
   // $('#content').bind("DOMSubtreeModified", function() {
   $('#content').bind("DOMNodeInserted DOMNodeRemoved", function() {
@@ -88,7 +118,7 @@ GooglePlayMusicAlbumSorter.prototype.init = function() {
     }
     domModifiedTimeout = setTimeout(function() {
       domModifiedCallback();
-    }, 1000);
+    }, 500);
 
   });
 };
